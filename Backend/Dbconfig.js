afd71e1a38a20f421a4cd912d528e2fa506f8a2c
alagunandhini <alagunadhini@gmail.com>
2026-01-14 16:27:56 +0530

    Fix dbConfig filename casing

--- Backend/Dbconfig.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = () => {
-  mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log("✅ MongoDB Connected"))
-    .catch(err => console.log("❌ MongoDB Error:", err));
-};
-
-module.exports = connectDB;
