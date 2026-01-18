312a5eded9684cd947ad7a2c46ca00be447b6c95
alagunandhini <alagunadhini@gmail.com>
2026-01-18 14:13:17 +0530

    Added requirements.txt for whisper dependencies

--- Backend/app.js
+++ Backend/app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require("dotenv").config();
 var cors = require('cors');
-const connectDB = require("./temp.js");
+const connectDB = require("./temp.js"); 
+
 
 
 
